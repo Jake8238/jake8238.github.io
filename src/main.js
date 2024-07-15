871ad1bf8e7ad1d18a5d92cddd6e7d63e3c8d28b
Jake8238 <meke2652@gmail.com>
2024-07-15 14:29:45 +0900

    JS-Arrowup

--- src/main.js
+++ src/main.js
@@ -13,6 +13,14 @@ const home = document.querySelector('.home__container');
 const homeHeight = home.offsetHeight;
 
 document.addEventListener('scroll', () => {
-  console.log(1 - window.scrollY / homeHeight);
   home.style.opacity = 1 - window.scrollY / homeHeight;
+});
+
+const arrow = document.querySelector('.arrow-up');
+document.addEventListener('scroll', () => {
+  if (window.scrollY < homeHeight / 2) {
+    arrow.style.opacity = 0;
+  } else {
+    arrow.style.opacity = 1;
+  }
 });
