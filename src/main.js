b0fe2a4af258555f39b266105e49f9d3b1a42fbb
Jake8238 <meke2652@gmail.com>
2024-07-17 12:58:39 +0900

    JS-File

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
+'use strict';
+
+// Header에 페이지 아래로 스크롤 시 다크 스타일링 적용
 const header = document.querySelector('.header');
 const headerHeight = header.getBoundingClientRect().height;
-
 document.addEventListener('scroll', () => {
   if (window.scrollY > headerHeight) {
     header.classList.add('header--dark');
@@ -9,9 +11,9 @@ document.addEventListener('scroll', () => {
   }
 });
 
+// Header 센셕을 아래로 스크롤 시 투명하게 처리
 const home = document.querySelector('.home__container');
 const homeHeight = home.offsetHeight;
-
 document.addEventListener('scroll', () => {
   home.style.opacity = 1 - window.scrollY / homeHeight;
 });
